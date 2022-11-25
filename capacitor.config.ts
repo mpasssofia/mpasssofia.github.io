1edbac2ed2c555bb82cd19289f5c4eee348ff055
Atanas Ivanov <mpass.sofia@gmail.com>
2022-11-25 12:50:39 +0200

    androd setup

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.github.mpasssofia',
+  appName: 'mpass',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
